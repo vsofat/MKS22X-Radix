eed7bcc99905c2542425a9bf3a96eb1befcbf221
vsofat <vsofat00@stuy.edu>
2019-04-11 11:59:27 -0400

    node

---
+++ Node.java
@@ -0,0 +1,54 @@
+public class Node<E> {
+  private E data;
+  private Node next,prev;
+
+@SuppressWarnings("unchecked")
+  public Node(E generic) { // should the next node be part of the constructor?
+    data = generic;
+  }
+
+@SuppressWarnings("unchecked")
+  public Node() {
+
+  }
+
+  public void setNext(Node setNext) {
+    next = setNext;
+  }
+
+  public void setPrev(Node setPrev) {
+    prev = setPrev;
+  }
+
+  public E getData() {
+    return data;
+  }
+
+  public E setData(E newData) {
+    E oldData = data;
+
+    data = newData;
+    return oldData;
+  }
+
+  public Node next() {
+    return next;
+  }
+
+  public Node prev() {
+    return prev;
+  }
+
+  public boolean hasNext() {
+    if (next != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String toString() {
+    return "" + getData();
+  }
+
+}
